269fdc91618019d698bfc360e7d961c02348afd5
nishit hegde <hegdenishit84@gmail.com>
2022-01-26 19:57:46 +0530

    adding tf file

---
+++ cicd-pipeline.tf
@@ -0,0 +1,101 @@
+resource "aws_codebuild_project" "tf-plan" {
+  name         = "tf-cicd-plan"
+  description  = "Plan stage for terraform"
+  service_role = aws_iam_role.tf-codebuild-role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = var.pipeline_container
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+  }
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = file("buildspec/plan-buildspec.yml")
+  }
+}
+
+resource "aws_codebuild_project" "tf-apply" {
+  name         = "tf-cicd-apply"
+  description  = "Apply stage for terraform"
+  service_role = aws_iam_role.tf-codebuild-role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = var.pipeline_container
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+  }
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = file("buildspec/apply-buildspec.yml")
+  }
+}
+
+
+resource "aws_codepipeline" "cicd_pipeline" {
+  depends_on = [aws_s3_bucket.codepipeline_artifacts]
+  name       = "tf-cicd"
+  role_arn   = aws_iam_role.tf-codepipeline-role.arn
+
+  artifact_store {
+    type     = "S3"
+    location = aws_s3_bucket.codepipeline_artifacts.id
+  }
+
+  stage {
+    name = "Source"
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeStarSourceConnection"
+      version          = "1"
+      output_artifacts = ["tf-code"]
+      configuration = {
+        FullRepositoryId     = "docomo-digital/pipeline-containers-lz"
+        BranchName           = "dev"
+        ConnectionArn        = var.codestar_connector_credentials
+        OutputArtifactFormat = "CODE_ZIP"
+      }
+    }
+  }
+
+  stage {
+    name = "Plan"
+    action {
+      name            = "Plan"
+      category        = "Build"
+      provider        = "CodeBuild"
+      version         = "1"
+      owner           = "AWS"
+      input_artifacts = ["tf-code"]
+      configuration = {
+        ProjectName = "tf-cicd-plan"
+      }
+    }
+  }
+
+  stage {
+    name = "Apply"
+    action {
+      name            = "Apply"
+      category        = "Build"
+      provider        = "CodeBuild"
+      version         = "1"
+      owner           = "AWS"
+      input_artifacts = ["tf-code"]
+      configuration = {
+        ProjectName = "tf-cicd-apply"
+      }
+    }
+  }
+}
